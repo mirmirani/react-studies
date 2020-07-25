fcf5f573f72c56873b3d06ad0c256bf9b88175ea
Mir Mirani <mir.mirani@outlook.com>
2020-07-25 10:01:28 +0100

    react-notes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+//import ReactDOM from "react-dom";
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Finally!!! <br />
+          REACT learning started!
         </p>
         <a
           className="App-link"
@@ -21,6 +22,9 @@ function App() {
       </header>
     </div>
   );
+
 }
+
+
 
 export default App;
